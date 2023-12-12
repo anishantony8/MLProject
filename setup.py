0bf6fe2f4806c59cec968b3dc808fe28a1c3a560
anishantony8 <95241589+anishantony8@users.noreply.github.com>
2023-12-12 10:27:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Anish Antony',
+author_email='anishantony8@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
